6cf43889599d1419f1191ae29d23525ca1112d6c
cassandraortiz <cassy.ortiz5@gmail.com>
2020-09-21 13:09:45 -0600

    Lesson Repo did not clone.  Completed Lesson work here.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,44 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+// Demonstrate Listening to Events on a DOM Node with addEventListener()
+const main = document.getElementById('main');
+
+main.addEventListener('click', function(event){
+    alert('I was clicked');
+})
+
+// Triggering Events Listeners on DOM Nodes
+const input = document.querySelector('input');
+
+// input.addEventListener('keydown', function(e){
+//     console.log(e.key);
+// })
+
+// Demonstrate Preventing the Default Behavior for DOM Nodes
+input.addEventListener('keydown', function(e){
+    if (e.key === 'g'){
+        return e.preventDefault() //Stops shit from working
+    } else {
+        console.log(e.key);
+    }
+})
+
+// Bubbling and Capturing Events
+let divs = document.querySelectorAll('div');
+
+function bubble(e) {
+    e.stopPropagation();
+    console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i <divs.length; i++){
+    divs[i].addEventListener('click',bubble);
+}
+
+// Capturing Events
+function captured(e) {
+    console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+
+for (let i = 0; i <divs.length; i++){
+    divs[i].addEventListener('click',captured,true);
+}
+
